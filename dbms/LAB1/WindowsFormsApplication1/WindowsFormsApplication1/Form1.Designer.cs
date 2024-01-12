b8ba82add27950717dcb043763574bd0d38089d6
Lepnoxic <41071854+lepnoxic@users.noreply.github.com>
2024-01-12 11:03:29 +0530

    Add files via upload

---
+++ dbms/LAB1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addbutton = new System.Windows.Forms.Button();
+            this.subbutton = new System.Windows.Forms.Button();
+            this.mulbutton = new System.Windows.Forms.Button();
+            this.numpad7 = new System.Windows.Forms.Button();
+            this.divbutton = new System.Windows.Forms.Button();
+            this.numpad0 = new System.Windows.Forms.Button();
+            this.numpad8 = new System.Windows.Forms.Button();
+            this.numpad3 = new System.Windows.Forms.Button();
+            this.numpad2 = new System.Windows.Forms.Button();
+            this.numpad1 = new System.Windows.Forms.Button();
+            this.numpad6 = new System.Windows.Forms.Button();
+            this.numpad5 = new System.Windows.Forms.Button();
+            this.numpad4 = new System.Windows.Forms.Button();
+            this.numpad9 = new System.Windows.Forms.Button();
+            this.tanbutton = new System.Windows.Forms.Button();
+            this.logbutton = new System.Windows.Forms.Button();
+            this.rootbutton = new System.Windows.Forms.Button();
+            this.sinbutton = new System.Windows.Forms.Button();
+            this.equalbutton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // addbutton
+            // 
+            this.addbutton.Location = new System.Drawing.Point(13, 131);
+            this.addbutton.Name = "addbutton";
+            this.addbutton.Size = new System.Drawing.Size(33, 22);
+            this.addbutton.TabIndex = 1;
+            this.addbutton.Text = "+";
+            this.addbutton.UseVisualStyleBackColor = true;
+            this.addbutton.Click += new System.EventHandler(this.addbutton_Click);
+            // 
+            // subbutton
+            // 
+            this.subbutton.Location = new System.Drawing.Point(13, 159);
+            this.subbutton.Name = "subbutton";
+            this.subbutton.Size = new System.Drawing.Size(33, 22);
+            this.subbutton.TabIndex = 2;
+            this.subbutton.Text = "-";
+            this.subbutton.UseVisualStyleBackColor = true;
+            this.subbutton.Click += new System.EventHandler(this.subbutton_Click);
+            // 
+            // mulbutton
+            // 
+            this.mulbutton.Location = new System.Drawing.Point(13, 187);
+            this.mulbutton.Name = "mulbutton";
+            this.mulbutton.Size = new System.Drawing.Size(33, 22);
+            this.mulbutton.TabIndex = 3;
+            this.mulbutton.Text = "*";
+            this.mulbutton.UseVisualStyleBackColor = true;
+            this.mulbutton.Click += new System.EventHandler(this.mulbutton_Click);
+            // 
+            // numpad7
+            // 
+            this.numpad7.Location = new System.Drawing.Point(127, 187);
+            this.numpad7.Name = "numpad7";
+            this.numpad7.Size = new System.Drawing.Size(32, 22);
+            this.numpad7.TabIndex = 8;
+            this.numpad7.Text = "7";
+            this.numpad7.UseVisualStyleBackColor = true;
+            this.numpad7.Click += new System.EventHandler(this.numpad7_Click);
+            // 
+            // divbutton
+            // 
+            this.divbutton.Location = new System.Drawing.Point(13, 215);
+            this.divbutton.Name = "divbutton";
+            this.divbutton.Size = new System.Drawing.Size(33, 22);
+            this.divbutton.TabIndex = 9;
+            this.divbutton.Text = "/";
+            this.divbutton.UseVisualStyleBackColor = true;
+            this.divbutton.Click += new System.EventHandler(this.divbutton_Click_1);
+            // 
+            // numpad0
+            // 
+            this.numpad0.Location = new System.Drawing.Point(127, 215);
+            this.numpad0.Name = "numpad0";
+            this.numpad0.Size = new System.Drawing.Size(32, 22);
+            this.numpad0.TabIndex = 10;
+            this.numpad0.Text = "0";
+            this.numpad0.UseVisualStyleBackColor = true;
+            this.numpad0.Click += new System.EventHandler(this.numpad0_Click);
+            // 
+            // numpad8
+            // 
+            this.numpad8.Location = new System.Drawing.Point(165, 187);
+            this.numpad8.Name = "numpad8";
+            this.numpad8.Size = new System.Drawing.Size(32, 22);
+            this.numpad8.TabIndex = 11;
+            this.numpad8.Text = "8";
+            this.numpad8.UseVisualStyleBackColor = true;
+            this.numpad8.Click += new System.EventHandler(this.numpad8_Click);
+            // 
+            // numpad3
+            // 
+            this.numpad3.Location = new System.Drawing.Point(203, 131);
+            this.numpad3.Name = "numpad3";
+            this.numpad3.Size = new System.Drawing.Size(32, 22);
+            this.numpad3.TabIndex = 12;
+            this.numpad3.Text = "3";
+            this.numpad3.UseVisualStyleBackColor = true;
+            this.numpad3.Click += new System.EventHandler(this.numpad3_Click);
+            // 
+            // numpad2
+            // 
+            this.numpad2.Location = new System.Drawing.Point(165, 131);
+            this.numpad2.Name = "numpad2";
+            this.numpad2.Size = new System.Drawing.Size(32, 22);
+            this.numpad2.TabIndex = 13;
+            this.numpad2.Text = "2";
+            this.numpad2.UseVisualStyleBackColor = true;
+            this.numpad2.Click += new System.EventHandler(this.numpad2_Click);
+            // 
+            // numpad1
+            // 
+            this.numpad1.Location = new System.Drawing.Point(127, 131);
+            this.numpad1.Name = "numpad1";
+            this.numpad1.Size = new System.Drawing.Size(32, 22);
+            this.numpad1.TabIndex = 14;
+            this.numpad1.Text = "1";
+            this.numpad1.UseVisualStyleBackColor = true;
+            this.numpad1.Click += new System.EventHandler(this.numpad1_Click);
+            // 
+            // numpad6
+            // 
+            this.numpad6.Location = new System.Drawing.Point(203, 159);
+            this.numpad6.Name = "numpad6";
+            this.numpad6.Size = new System.Drawing.Size(32, 22);
+            this.numpad6.TabIndex = 15;
+            this.numpad6.Text = "6";
+            this.numpad6.UseVisualStyleBackColor = true;
+            this.numpad6.Click += new System.EventHandler(this.numpad6_Click);
+            // 
+            // numpad5
+            // 
+            this.numpad5.Location = new System.Drawing.Point(165, 159);
+            this.numpad5.Name = "numpad5";
+            this.numpad5.Size = new System.Drawing.Size(32, 22);
+            this.numpad5.TabIndex = 16;
+            this.numpad5.Text = "5";
+            this.numpad5.UseVisualStyleBackColor = true;
+            this.numpad5.Click += new System.EventHandler(this.numpad5_Click);
+            // 
+            // numpad4
+            // 
+            this.numpad4.Location = new System.Drawing.Point(127, 159);
+            this.numpad4.Name = "numpad4";
+            this.numpad4.Size = new System.Drawing.Size(32, 22);
+            this.numpad4.TabIndex = 17;
+            this.numpad4.Text = "4";
+            this.numpad4.UseVisualStyleBackColor = true;
+            this.numpad4.Click += new System.EventHandler(this.numpad4_Click);
+            // 
+            // numpad9
+            // 
+            this.numpad9.Location = new System.Drawing.Point(203, 187);
+            this.numpad9.Name = "numpad9";
+            this.numpad9.Size = new System.Drawing.Size(32, 22);
+            this.numpad9.TabIndex = 18;
+            this.numpad9.Text = "9";
+            this.numpad9.UseVisualStyleBackColor = true;
+            this.numpad9.Click += new System.EventHandler(this.numpad9_Click);
+            // 
+            // tanbutton
+            // 
+            this.tanbutton.Location = new System.Drawing.Point(52, 159);
+            this.tanbutton.Name = "tanbutton";
+            this.tanbutton.Size = new System.Drawing.Size(33, 22);
+            this.tanbutton.TabIndex = 22;
+            this.tanbutton.Text = "tan";
+            this.tanbutton.UseVisualStyleBackColor = true;
+            this.tanbutton.Click += new System.EventHandler(this.tanbutton_Click);
+            // 
+            // logbutton
+            // 
+            this.logbutton.Location = new System.Drawing.Point(52, 187);
+            this.logbutton.Name = "logbutton";
+            this.logbutton.Size = new System.Drawing.Size(33, 22);
+            this.logbutton.TabIndex = 21;
+            this.logbutton.Text = "log";
+            this.logbutton.UseVisualStyleBackColor = true;
+            this.logbutton.Click += new System.EventHandler(this.logbutton_Click);
+            // 
+            // rootbutton
+            // 
+            this.rootbutton.Location = new System.Drawing.Point(52, 215);
+            this.rootbutton.Name = "rootbutton";
+            this.rootbutton.Size = new System.Drawing.Size(33, 22);
+            this.rootbutton.TabIndex = 20;
+            this.rootbutton.Text = "root";
+            this.rootbutton.UseVisualStyleBackColor = true;
+            this.rootbutton.Click += new System.EventHandler(this.rootbutton_Click);
+            // 
+            // sinbutton
+            // 
+            this.sinbutton.Location = new System.Drawing.Point(52, 131);
+            this.sinbutton.Name = "sinbutton";
+            this.sinbutton.Size = new System.Drawing.Size(33, 22);
+            this.sinbutton.TabIndex = 19;
+            this.sinbutton.Text = "sin";
+            this.sinbutton.UseVisualStyleBackColor = true;
+            this.sinbutton.Click += new System.EventHandler(this.sinbutton_Click);
+            // 
+            // equalbutton
+            // 
+            this.equalbutton.Location = new System.Drawing.Point(166, 215);
+            this.equalbutton.Name = "equalbutton";
+            this.equalbutton.Size = new System.Drawing.Size(69, 22);
+            this.equalbutton.TabIndex = 24;
+            this.equalbutton.Text = "=";
+            this.equalbutton.UseVisualStyleBackColor = true;
+            this.equalbutton.Click += new System.EventHandler(this.equalbutton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 94);
+            this.textBox1.TabIndex = 25;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.equalbutton);
+            this.Controls.Add(this.tanbutton);
+            this.Controls.Add(this.logbutton);
+            this.Controls.Add(this.rootbutton);
+            this.Controls.Add(this.sinbutton);
+            this.Controls.Add(this.numpad9);
+            this.Controls.Add(this.numpad4);
+            this.Controls.Add(this.numpad5);
+            this.Controls.Add(this.numpad6);
+            this.Controls.Add(this.numpad1);
+            this.Controls.Add(this.numpad2);
+            this.Controls.Add(this.numpad3);
+            this.Controls.Add(this.numpad8);
+            this.Controls.Add(this.numpad0);
+            this.Controls.Add(this.divbutton);
+            this.Controls.Add(this.numpad7);
+            this.Controls.Add(this.mulbutton);
+            this.Controls.Add(this.subbutton);
+            this.Controls.Add(this.addbutton);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addbutton;
+        private System.Windows.Forms.Button subbutton;
+        private System.Windows.Forms.Button mulbutton;
+        private System.Windows.Forms.Button numpad7;
+        private System.Windows.Forms.Button divbutton;
+        private System.Windows.Forms.Button numpad0;
+        private System.Windows.Forms.Button numpad8;
+        private System.Windows.Forms.Button numpad3;
+        private System.Windows.Forms.Button numpad2;
+        private System.Windows.Forms.Button numpad1;
+        private System.Windows.Forms.Button numpad6;
+        private System.Windows.Forms.Button numpad5;
+        private System.Windows.Forms.Button numpad4;
+        private System.Windows.Forms.Button numpad9;
+        private System.Windows.Forms.Button tanbutton;
+        private System.Windows.Forms.Button logbutton;
+        private System.Windows.Forms.Button rootbutton;
+        private System.Windows.Forms.Button sinbutton;
+        private System.Windows.Forms.Button equalbutton;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ dbms/LAB1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        string s = "";
+        public static string Evaluate(string expression)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            table.Columns.Add("expression", string.Empty.GetType(), expression);
+            System.Data.DataRow row = table.NewRow();
+            table.Rows.Add(row);
+            return (string)row["expression"];
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numpad1_Click(object sender, EventArgs e)
+        {
+            s += "1";
+            textBox1.Text = s;
+        }
+
+        private void sinbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s = Math.Sin(double.Parse(Evaluate(s))).ToString();
+                textBox1.Text = s;
+                s = "";
+            }
+            catch (Exception exc)
+            {
+                textBox1.Text = "Invalid!";
+                s = "";
+            }
+            
+        }
+
+        private void rootbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s = Math.Sqrt(double.Parse(Evaluate(s))).ToString();
+                textBox1.Text = s;
+                s = "";
+            }
+            catch (Exception exc)
+            {
+                textBox1.Text = "Invalid!";
+                s = "";
+            }
+        }
+
+        private void equalbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = Evaluate(s);
+                s = "";
+            }
+            catch (Exception exc)
+            {
+                textBox1.Text = "Invalid!";
+                s = "";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mulbutton_Click(object sender, EventArgs e)
+        {
+            s += " * ";
+            textBox1.Text = s;
+        }
+
+        private void subbutton_Click(object sender, EventArgs e)
+        {
+            s += " - ";
+            textBox1.Text = s;
+        }
+
+        private void addbutton_Click(object sender, EventArgs e)
+        {
+            s += " + ";
+            textBox1.Text = s;
+        }
+
+        private void divbutton_Click_1(object sender, EventArgs e)
+        {
+            s += " / ";
+            textBox1.Text = s;
+        }
+
+        private void numpad2_Click(object sender, EventArgs e)
+        {
+            s += "2";
+            textBox1.Text = s;
+        }
+
+        private void numpad3_Click(object sender, EventArgs e)
+        {
+            s += "3";
+            textBox1.Text = s;
+        }
+
+        private void numpad4_Click(object sender, EventArgs e)
+        {
+            s += "4";
+            textBox1.Text = s;
+        }
+
+        private void numpad5_Click(object sender, EventArgs e)
+        {
+            s += "5";
+            textBox1.Text = s;
+        }
+
+        private void numpad6_Click(object sender, EventArgs e)
+        {
+            s += "6";
+            textBox1.Text = s;
+        }
+
+        private void numpad7_Click(object sender, EventArgs e)
+        {
+            s += "7";
+            textBox1.Text = s;
+        }
+
+        private void numpad8_Click(object sender, EventArgs e)
+        {
+            s += "8";
+            textBox1.Text = s;
+        }
+
+        private void numpad9_Click(object sender, EventArgs e)
+        {
+            s += "9";
+            textBox1.Text = s;
+        }
+
+        private void numpad0_Click(object sender, EventArgs e)
+        {
+            s += "0";
+            textBox1.Text = s;
+        }
+
+        private void tanbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s = Math.Tan(double.Parse(Evaluate(s))).ToString();
+                textBox1.Text = s;
+                s = "";
+            }
+            catch (Exception exc)
+            {
+                textBox1.Text = "Invalid!";
+                s = "";
+            }
+        }
+
+        private void logbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s = Math.Log10(double.Parse(Evaluate(s))).ToString();
+                textBox1.Text = s;
+                s = "";
+            }
+            catch (Exception exc)
+            {
+                textBox1.Text = "Invalid!";
+                s = "";
+            }
+        }
+    }
+}
